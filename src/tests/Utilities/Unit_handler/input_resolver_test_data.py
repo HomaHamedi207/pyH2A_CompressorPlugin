b49bf2cf6c6b875c5855193c6fe3143958ba5ec3
axfache <axel.fache@uni-jena.de>
2026-01-27 10:44:17 +0100

    Address the review'S comments: - unify the format of the input_dict - add examples where the value is a dict of floats or of arrays - use our standardized <...> notation for table groups or unknown second key - use the "item_schema" logic (we can rename later) - add import numpy as np - make sure a given upper key can have multiple mid key, each having its own type.

--- src/tests/Utilities/Unit_handler/input_resolver_test_data.py
+++ src/tests/Utilities/Unit_handler/input_resolver_test_data.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class TestingDCF:
     def __init__(self):
@@ -8,14 +9,15 @@ class TestingDCF:
             'Catalyst': {'Lifetime': {'Value': 5, 'Unit': 'years'}} ,
             'Catalyst Separation': {'Filtration cost': {'Value': 100, 'Unit': '$/m3'}},
             'Grid electricity': {'Cost': {'Value': 0.15, 'Unit': '$/kWh'}},
+            # The following one's values are dictionaries of floats or of arrays of floats
             'Power Generation': {
-                                'Available Power': {'Value': np.array([400., 250., 350.]), 'Unit': 'kWh'},
-                                'Stored Power': {'Value': np.array([200., 50., 150.]), 'Unit': 'kWh'}
+                                'Available Power': {'Value': {'2025':np.array([400., 250., 350.]), '2024':np.array([500., 350., 450.])}, 'Unit': 'kWh'},
+                                'Stored Power': {'Value': {'2025': 200., '2024': 250.}, 'Unit': 'kWh'}
                                 },
             # the following one serves to test the situation where middle key isn't known in advance / when we want to iterate
             'Power Consumption': {
                 'Any mid key': {'Value': np.array([100., 120., 110.]),'Type': 'on demand','Unit': 'kWh'},
-                'Another mid key': {'Value': np.array([50., 40., 60.]), 'Type': 'flexible','Unit': 'kWh'}                             
+                'Another mid key': {'Value': np.array([50., 40., 60.]), 'Type': 'flexible','Unit': 'kWh'},
                                 }, 
             # Pump and compressor: to test the "Other Variable Operating Cost" group
             'Pump - Other Variable Operating Cost - Brand A': {
@@ -31,99 +33,150 @@ class TestingDCF:
 
 input_dict = {
     'water_volume': {'top_level': 'Water Volume',
-                     'mid_level': 'Volume',
-                     'lower_level': 'Value',
-                     'dimension': 'volume',
-                     'type': float,
-                     'optional': False,
-                     'bounds': (0, None),
-                     'description': 'Total water volume in liters.'},
-                     
-    'Bag_number': {'top_level': 'Number of units',
-                     'mid_level': 'Bag number',
-                     'lower_level': 'Value',
-                     'dimension': 'dimensionless',
-                     'type': int,
-                     'optional': False,
-                     'bounds': (0, None),
-                     'description': 'Number of bags in the system.'},
-                     
-    'Catalyst_lifetime': {'top_level': 'Catalyst',
-                     'mid_level': 'Lifetime',
-                     'lower_level': 'Value',
-                     'dimension': 'time',
-                     'type': float,
-                     'optional': False,
-                     'bounds': (0, None),
-                     'description': 'Catalyst lifetime in years.'},
-                     
-    'Catalyst_Separation': {'top_level': 'Catalyst Separation',
-                     'mid_level': 'Filtration cost',
-                     'lower_level': 'Value',
-                     'dimension': 'money_per_volume',
-                     'type': float,
-                     'optional': False,
-                     'bounds': (0, None),
-                     'description': 'Specific Catalyst separation cost.'},        
-
-    'power_available': {'top_level': 'Power Generation',
-                    'mid_level': 'Available Power',
-                    'lower_level': 'Value',
-                    'dimension': 'energy',
-                    'type': np.ndarray,
-                    'optional': False,
-                    'bounds': (0, None),
-                    'description': 'Available electric energy per period (length 3).'},                 
-                      
-    'grid_electricity_cost': {'top_level': 'Grid Electricity',
-                     'mid_level': 'Cost',
-                     'lower_level': 'Value',
-                     'dimension': 'money_per_energy',
-                     'type': {float, np.ndarray}, # will allow to check if the observed type of an instance matches an element of the set. I wonder if we should declare it as a tuple (np.ndarray, float) so we can chack: if it's an array (first tuple element), are the elements of the array floats (second tuple element)?
-                     'optional': True,
-                     'bounds': (0, None),
-                     'description': 'Cost of grid electricity in $/kWh'},        
-   
-                    
-    'power_stored': {'top_level': 'Power Generation',
-                    'mid_level': 'Stored Power',
-                    'lower_level': 'Value',
-                    'dimension': 'energy',
-                    'type': np.ndarray,
-                    'optional': True,
-                    'bounds': (0, None),
-                    'description': 'Stored electric energy per period (length 3).'}, 
-    
-    'power_consumption': {'top_level': 'Power Consumption',
-                    'kind': 'collection', # refers to the case when the middle key isn't known in advance
-                    'item_schema': {
-                        'Value': {
-                            'type': np.ndarray,
-                            'bounds': (0, None),
-                            'length': 3,
-                            'dimension': 'energy'
+                         'mid_level' : {
+                            'Volume':{
+                                'item_schema': {                    
+                                    'Value': {
+                                        'type': {float,},
+                                        'bounds': (0, None),
+                                    },
+                                    'Unit': {'dimension': 'volume',},                    
+                                 },
+                                'optional': False,
+                                'description': 'Total water volume in liters.'
+                            },                                                        
                         },
-                        'Type': {
-                            'type': str,
-                            'allowed': {'on demand', 'flexible'}
-                        }
-                    },
-                    'optional': True,
-                    'description': 'Power consumption per consumer.'},                   
+    },
+    
+    'Bag_number': {'top_level': 'Number of units',
+                         'mid_level' : {
+                            'Bag number':{
+                                'item_schema': {                    
+                                    'Value': {
+                                        'type': {int,},
+                                        'bounds': (0, None),
+                                    },
+                                    'Unit': {'dimension': 'dimensionless',},                    
+                                 },
+                                'optional': False,
+                                'description': 'Number of bags in the system.'
+                            },                                                        
+                        },
+    },
 
-    'other_variable_operating_cost': {
-                    'table_group': 'Other Variable Operating Cost', # Pattern to be recognized as being in the group 
-                    'kind': 'collection',
-                    'item_schema': {
-                        'Value': {
-                            'type': {float, np.ndarray},
-                            'bounds': (0, None),
-                            'dimension': 'money'
-                        }
-                    },
-                    'optional': True,
-                    'description': 'All other variable operating costs.'}
-                     
-    }
+    'Catalyst_lifetime': {'top_level': 'Catalyst',
+                         'mid_level' : {
+                            'Lifetime':{
+                                'item_schema': {                    
+                                    'Value': {
+                                        'type': {float,},
+                                        'bounds': (0, None),
+                                    },
+                                    'Unit': {'dimension': 'time',},                    
+                                 },
+                                'optional': False,
+                                'description': 'Catalyst lifetime in years.'
+                            },                                                        
+                        },
+    },
+
+    'Catalyst_Separation': {'top_level': 'Catalyst Separation',
+                         'mid_level' : {
+                            'Filtration cost':{
+                                'item_schema': {                    
+                                    'Value': {
+                                        'type': {float,},
+                                        'bounds': (0, None),
+                                    },
+                                    'Unit': {'dimension': 'money_per_volume',},                    
+                                 },
+                                'optional': False,
+                                'description': 'Specific Catalyst separation cost.'
+                            },                                                        
+                        },
+    },
+    
+    'grid_electricity_cost': {'top_level': 'Grid Electricity',
+                         'mid_level' : {
+                            'Cost':{
+                                'item_schema': {                    
+                                    'Value': {
+                                        'type': {float, np.ndarray}, # I kept things simple for the moment: just check that the value is one of the types specified in the set. We don't check the type of the eventual nested structure
+                                        'bounds': (0, None),
+                                    },
+                                    'Unit': {'dimension': 'money_per_energy',},                    
+                                 },
+                                'optional': True,
+                                'description': 'Stored electric energy per period.'
+                            },                                                        
+                        },
+    },
+
+    'Power_Generation': {'top_level': 'Power Generation',
+                         'mid_level' : {
+                            'Stored Power':{
+                                'item_schema': {                    
+                                    'Value': {
+                                        'type': {dict,},
+                                        'bounds': (0, None),
+                                    },
+                                    'Unit': {'dimension': 'energy',},                    
+                                 },
+                                'optional': True,
+                                'description': 'Stored electric energy per period.'
+                            },
+                            'Available Power':{
+                                'item_schema': {                    
+                                    'Value': {
+                                        'type': {dict,}, 
+                                        'bounds': (0, None),
+                                    },
+                                    'Unit': {'dimension': 'energy',},                    
+                                },     
+                                'optional': False,
+                                'description': 'Available electric energy per period.'
+                            },                                                         
+                        },
+    },
+
+    'power_consumption': {'top_level': 'Power Consumption',
+                         'mid_level' : {
+                            '<...>':{ # refers to the case when the middle key isn't known in advance
+                                'item_schema': {                    # Assuming all the keys that are not known in advance must follow the same schema
+                                    'Value': {
+                                        'type': {np.ndarray,},
+                                        'bounds': (0, None),
+                                        'length': 3 # expected length of the array (in practice, would be more like 365 for a value per day of the year)
+                                    },
+                                    'Unit': {'dimension': 'energy',},   
+                                    'Type': {
+                                        'type': {str,},
+                                        'options': {'on demand', 'flexible'},                                    
+                                    },
+                                },                                     
+                                'optional': True,
+                                'description': 'Power consumption per consumer.'
+                            },                                                        
+                        },
+    },
+    
+     'other_variable_operating_cost': {'top_level': '<...> Other Variable Operating Cost <...>', # table group
+                         'mid_level' : {
+                            '<...>':{ 
+                                'item_schema': {                    
+                                    'Value': {
+                                        'type': {float, np.ndarray},
+                                        'bounds': (0, None),
+                                        # length might not be known in advance: check its consistency only if the 'length' key is found in the input
+                                    },
+                                    'Unit': {'dimension': 'money',},   
+                                },                                     
+                                'optional': True,
+                                'description': 'All other variable operating costs.'
+                            },                                                        
+                        },
+    }         
+                    
+}
 
                      
