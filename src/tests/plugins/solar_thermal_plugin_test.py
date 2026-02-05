64315e7a7ba147366658e88afc8b72dce731b545
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-02-05 02:28:08 +0100

    test: add unit test for Solar Thermal Plugin.

---
+++ src/tests/plugins/solar_thermal_plugin_test.py
@@ -0,0 +1,41 @@
+import pytest
+from pyH2A.Plugins.Solar_Thermal_Plugin import Solar_Thermal_Plugin
+
+class DummyDCF:
+    """DummyDCF for Solar Thermal Plugin for unit testing"""
+    def __init__(
+        self,
+        design_output_per_day,
+        sth_efficiency,
+        mean_solar_input,
+        additional_land_area
+        ):
+        self.inp = {
+            "Technical Operating Parameters and Specifications": {"Design Output per Day": {"Value": design_output_per_day}},
+            "Solar-to-Hydrogen Efficiency": {"STH (%)": {"Value": sth_efficiency}},
+            "Solar Input": {"Mean solar input (kWh/m2/day)": {"Value":mean_solar_input }},
+            "Non-Depreciable Capital Costs": {"Additional Land Area (%)": {"Value": additional_land_area}}
+        }
+        
+@pytest.mark.parametrize(
+    "case",
+    [
+        {
+            "input":
+                {
+                    "design_output_per_day": 1000,
+                    "sth_efficiency": 0.14,
+                    "mean_solar_input": 5.499228123213646,
+                    "additional_land_area": 0,
+                },
+            "expected": 10.572124480592073
+        }
+    ],
+)
+
+def test_solar_thermal_plugin(case):
+    """Check plugin handles edge and real cases without errors and returns correct annualized costs."""
+    dcf = DummyDCF(**case["input"])
+    plugin = Solar_Thermal_Plugin(dcf, print_info=False)
+    
+    assert plugin.area_acres == case["expected"]
