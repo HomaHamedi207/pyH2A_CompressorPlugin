50b9939e1372b2e2b36d97f52f3b697110870bd0
HomaHamedi_HWU <hh4014@hw.ac.uk>
2026-02-20 22:02:29 +0100

    [feat] Runner Script

---
+++ src/pyH2A/Runner.py
@@ -0,0 +1,10 @@
+from pyH2A.run_pyH2A import pyH2A
+
+result = pyH2A('src/pyH2A/Example/190226_PEC_Type_1_Comp.md', '.')
+
+
+#Options for input files:
+#src/pyH2A/Example/210511_Future_PEC_Type_4.md
+#211109_Future_PEC_Type_1_Figure_Test.md
+#210613_PV_E.md
+#190226_PEC_Type_1_Comp.md
